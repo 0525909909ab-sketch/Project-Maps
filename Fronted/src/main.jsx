5f9676ae6107a70958b4809e43e21064f90971d0
GUYro90 <0525909909ab@gmail.com>
2026-07-15 12:14:43 +0300

    Fix merge conflict in main.jsx

--- Fronted/src/main.jsx
+++ Fronted/src/main.jsx
@@ -1,13 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-<<<<<<< HEAD
-import App from './App.jsx'
-import 'mapbox-gl/dist/mapbox-gl.css';
-
-=======
 import './index.css'
 import App from './App.jsx'
->>>>>>> origin/main
+import 'mapbox-gl/dist/mapbox-gl.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
