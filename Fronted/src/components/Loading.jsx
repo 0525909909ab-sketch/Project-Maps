478ba53b6bffb5976a3504951cef2213404ff894
GUYro90 <0525909909ab@gmail.com>
2026-07-19 17:24:21 +0300

    Remove Fronted folder from remote repository

--- Fronted/src/components/Loading.jsx
+++
@@ -1,6 +0,0 @@
-const Loading=()=>{
-  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', fontFamily: 'sans-serif', direction: 'rtl' }}>
-        טוען את מפת התלת-ממד...
-      </div>
-}
-export default Loading
